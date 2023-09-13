bd52695192974b29944d9f4d572c99a0587011b2
TonichUA <132302438+TonichUA@users.noreply.github.com>
2023-09-13 21:16:16 +0000

    Deploying to gh-pages from @ TonichUA/goit-js-hw-08@276fdd6d9c0d4473924ac5931ab04ba1a210e179 🚀

--- 03-feedback.8d125b87.js
+++ 03-feedback.8d125b87.js
@@ -35,7 +35,7 @@ var e,t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:
  * @private
  * @param {string} string The string to trim.
  * @returns {string} Returns the trimmed string.
- */function(e){return e?e.slice(0,v(e)+1).replace(d,""):e};var m={},p={},g={};g=a.Symbol;var y={},b=Object.prototype,j=b.hasOwnProperty,S=b.toString,h=g?g.toStringTag:void 0;y=/**
+ */function(e){return e?e.slice(0,v(e)+1).replace(d,""):e};var m={},g={},p={};p=a.Symbol;var y={},b=Object.prototype,j=b.hasOwnProperty,S=b.toString,h=p?p.toStringTag:void 0;y=/**
  * A specialized version of `baseGetTag` which ignores `Symbol.toStringTag` values.
  *
  * @private
@@ -47,7 +47,7 @@ var e,t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:
  * @private
  * @param {*} value The value to convert.
  * @returns {string} Returns the converted string.
- */function(e){return T.call(e)};/** Built-in value references. */var w=g?g.toStringTag:void 0;p=/**
+ */function(e){return T.call(e)};/** Built-in value references. */var w=p?p.toStringTag:void 0;g=/**
  * The base implementation of `getTag` without fallbacks for buggy environments.
  *
  * @private
@@ -92,7 +92,7 @@ var e,t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:
  *
  * _.isSymbol('abc');
  * // => false
- */function(e){return"symbol"==typeof e||x(e)&&"[object Symbol]"==p(e)};/** Used as references for various `Number` constants. */var E=0/0,I=/^[-+]0x[0-9a-f]+$/i,q=/^0b[01]+$/i,L=/^0o[0-7]+$/i,M=parseInt;f=/**
+ */function(e){return"symbol"==typeof e||x(e)&&"[object Symbol]"==g(e)};/** Used as references for various `Number` constants. */var E=0/0,I=/^[-+]0x[0-9a-f]+$/i,q=/^0b[01]+$/i,M=/^0o[0-7]+$/i,N=parseInt;f=/**
  * Converts `value` to a number.
  *
  * @static
@@ -114,7 +114,7 @@ var e,t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:
  *
  * _.toNumber('3.2');
  * // => 3.2
- */function(e){if("number"==typeof e)return e;if(m(e))return E;if(i(e)){var t="function"==typeof e.valueOf?e.valueOf():e;e=i(t)?t+"":t}if("string"!=typeof e)return 0===e?e:+e;e=c(e);var n=q.test(e);return n||L.test(e)?M(e.slice(2),n?2:8):I.test(e)?E:+e};/* Built-in method references for those with the same name as other `lodash` methods. */var N=Math.max,W=Math.min;r=/**
+ */function(e){if("number"==typeof e)return e;if(m(e))return E;if(i(e)){var t="function"==typeof e.valueOf?e.valueOf():e;e=i(t)?t+"":t}if("string"!=typeof e)return 0===e?e:+e;e=c(e);var n=q.test(e);return n||M.test(e)?N(e.slice(2),n?2:8):I.test(e)?E:+e};/* Built-in method references for those with the same name as other `lodash` methods. */var W=Math.max,$=Math.min;r=/**
  * Creates a debounced function that delays invoking `func` until after `wait`
  * milliseconds have elapsed since the last time the debounced function was
  * invoked. The debounced function comes with a `cancel` method to cancel
@@ -167,16 +167,16 @@ var e,t="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:
  *
  * // Cancel the trailing debounced invocation.
  * jQuery(window).on('popstate', debounced.cancel);
- */function(e,t,n){var r,a,u,l,c,v,s=0,d=!1,m=!1,p=!0;if("function"!=typeof e)throw TypeError("Expected a function");function g(t){var n=r,i=a;return r=a=void 0,s=t,l=e.apply(i,n)}function y(e){var n=e-v,r=e-s;// Either this is the first call, activity has stopped and we're at the
+ */function(e,t,n){var r,a,u,l,c,v,s=0,d=!1,m=!1,g=!0;if("function"!=typeof e)throw TypeError("Expected a function");function p(t){var n=r,i=a;return r=a=void 0,s=t,l=e.apply(i,n)}function y(e){var n=e-v,r=e-s;// Either this is the first call, activity has stopped and we're at the
 // trailing edge, the system time has gone backwards and we're treating
 // it as the trailing edge, or we've hit the `maxWait` limit.
 return void 0===v||n>=t||n<0||m&&r>=u}function b(){var e,n,r,i=o();if(y(i))return j(i);// Restart the timer.
-c=setTimeout(b,(e=i-v,n=i-s,r=t-e,m?W(r,u-n):r))}function j(e){return(// Only invoke if we have `lastArgs` which means `func` has been
+c=setTimeout(b,(e=i-v,n=i-s,r=t-e,m?$(r,u-n):r))}function j(e){return(// Only invoke if we have `lastArgs` which means `func` has been
 // debounced at least once.
-(c=void 0,p&&r)?g(e):(r=a=void 0,l))}function S(){var e,n=o(),i=y(n);if(r=arguments,a=this,v=n,i){if(void 0===c)return(// Reset any `maxWait` timer.
+(c=void 0,g&&r)?p(e):(r=a=void 0,l))}function S(){var e,n=o(),i=y(n);if(r=arguments,a=this,v=n,i){if(void 0===c)return(// Reset any `maxWait` timer.
 s=e=v,// Start the timer for the trailing edge.
-c=setTimeout(b,t),d?g(e):l);if(m)return(// Handle invocations in a tight loop.
-clearTimeout(c),c=setTimeout(b,t),g(v))}return void 0===c&&(c=setTimeout(b,t)),l}return t=f(t)||0,i(n)&&(d=!!n.leading,u=(m="maxWait"in n)?N(f(n.maxWait)||0,t):u,p="trailing"in n?!!n.trailing:p),S.cancel=function(){void 0!==c&&clearTimeout(c),s=0,r=v=a=c=void 0},S.flush=function(){return void 0===c?l:j(o())},S},n=/**
+c=setTimeout(b,t),d?p(e):l);if(m)return(// Handle invocations in a tight loop.
+clearTimeout(c),c=setTimeout(b,t),p(v))}return void 0===c&&(c=setTimeout(b,t)),l}return t=f(t)||0,i(n)&&(d=!!n.leading,u=(m="maxWait"in n)?W(f(n.maxWait)||0,t):u,g="trailing"in n?!!n.trailing:g),S.cancel=function(){void 0!==c&&clearTimeout(c),s=0,r=v=a=c=void 0},S.flush=function(){return void 0===c?l:j(o())},S},n=/**
  * Creates a throttled function that only invokes `func` at most once per
  * every `wait` milliseconds. The throttled function comes with a `cancel`
  * method to cancel delayed `func` invocations and a `flush` method to
@@ -219,6 +219,6 @@ clearTimeout(c),c=setTimeout(b,t),g(v))}return void 0===c&&(c=setTimeout(b,t)),l
  *
  * // Cancel the trailing throttled invocation.
  * jQuery(window).on('popstate', throttled.cancel);
- */function(e,t,n){var o=!0,a=!0;if("function"!=typeof e)throw TypeError("Expected a function");return i(n)&&(o="leading"in n?!!n.leading:o,a="trailing"in n?!!n.trailing:a),r(e,t,{leading:o,maxWait:t,trailing:a})};const $=document.querySelector(".feedback-form"),k=$.querySelector('input[name="email"]'),D=$.querySelector('textarea[name="message"]');$.addEventListener("input",_);const J="feedback-form-state";function _(){let e={email:k.value,message:D.value};localStorage.setItem(J,JSON.stringify(e))}const A=((e=n)&&e.__esModule?e.default:e)(_,500);$.addEventListener("input",A),function(){let e=localStorage.getItem(J);if(e){let t=JSON.parse(e);k.value=t.email,D.value=t.message}else k.value="",D.value=""}(),$.addEventListener("submit",function(e){e.preventDefault();let t={email:k.value,message:D.value};console.log(t),k.value="",D.value="",localStorage.removeItem(J)}//# sourceMappingURL=03-feedback.0e4c806c.js.map
+ */function(e,t,n){var o=!0,a=!0;if("function"!=typeof e)throw TypeError("Expected a function");return i(n)&&(o="leading"in n?!!n.leading:o,a="trailing"in n?!!n.trailing:a),r(e,t,{leading:o,maxWait:t,trailing:a})};const k=document.querySelector(".feedback-form"),D=k.querySelector('input[name="email"]'),J=k.querySelector('textarea[name="message"]'),L="feedback-form-state",_=((e=n)&&e.__esModule?e.default:e)(function(){let e={email:D.value,message:J.value};localStorage.setItem(L,JSON.stringify(e))},500);k.addEventListener("input",_),function(){let e=localStorage.getItem(L);if(e){let t=JSON.parse(e);D.value=t.email,J.value=t.message}else D.value="",J.value=""}(),k.addEventListener("submit",function(e){e.preventDefault();let t={email:D.value,message:J.value};console.log(t),D.value="",J.value="",localStorage.removeItem(L)}//# sourceMappingURL=03-feedback.8d125b87.js.map
 );
-//# sourceMappingURL=03-feedback.0e4c806c.js.map
+//# sourceMappingURL=03-feedback.8d125b87.js.map
